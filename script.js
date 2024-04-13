e481c2f933390787f96d65d7c2210c32dac8282c
Karim Feki <karimfeki2004@gmail.com>
2024-04-13 20:09:40 +0200

    error

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.style.opacity = 1;
-        entry.target.style.display = 'block';
       } else {
         entry.target.style.opacity = 0;
+        entry.target.style.display = 'block';
       }
     });
   }, options);
